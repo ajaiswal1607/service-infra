a55be1fe20774ae61ce4d18b10de35a574312fe8
ajaiswal1607 <er.jaiswalanand@gmail.com>
2026-08-07 18:15:23 +0530

    initial setup

--- common/bootstrap-state/main.tf
+++ common/bootstrap-state/main.tf
@@ -25,3 +25,54 @@ resource "aws_s3_bucket_public_access_block" "terraform_state" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+resource "aws_iam_role" "github_terraform" {
+  name = "anand-app-dev-github-terraform"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [{
+      Effect = "Allow"
+
+      Principal = {
+        Federated = "arn:aws:iam::359013545273:oidc-provider/token.actions.githubusercontent.com"
+      }
+
+      Action = [
+        "sts:AssumeRoleWithWebIdentity"
+      ]
+
+      Condition = {
+        StringEquals = {
+          "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+        }
+
+        StringLike = {
+          "token.actions.githubusercontent.com:sub" = "repo:ajaiswal1607/service-infra:*"
+        }
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "terraform_admin" {
+  role       = aws_iam_role.github_terraform.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+data "tls_certificate" "github" {
+  url = "https://token.actions.githubusercontent.com"
+}
+
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com"
+  ]
+
+  thumbprint_list = [
+    data.tls_certificate.github.certificates[0].sha1_fingerprint
+  ]
+}
--- common/bootstrap-state/outputs.tf
+++ common/bootstrap-state/outputs.tf
@@ -1,3 +1,19 @@
-output "state_bucket" {
-  value = aws_s3_bucket.terraform_state.bucket
+output "terraform_role_arn" {
+  description = "IAM role ARN used by GitHub Actions for Terraform"
+  value       = aws_iam_role.github_terraform.arn
+}
+
+output "terraform_role_name" {
+  description = "IAM role name used by GitHub Actions for Terraform"
+  value       = aws_iam_role.github_terraform.name
+}
+
+output "github_oidc_provider_arn" {
+  description = "GitHub Actions OIDC provider ARN"
+  value       = aws_iam_openid_connect_provider.github.arn
+}
+
+output "terraform_state_bucket" {
+  description = "Terraform state S3 bucket"
+  value       = aws_s3_bucket.terraform_state.bucket
 }
